e69ee4172826b253ff7d8075f09fb01557b8a9b6
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2010-07-12 10:28:17 +0300

    Added the relevant migration

---
+++ db/migrate/20100712065338_counter_add_finished_count.rb
@@ -0,0 +1,12 @@
+class CounterAddFinishedCount < ActiveRecord::Migration
+  def self.up
+    add_column :counters, :finished_tasks_num, :integer
+    add_column :counters, :total_tasks_num, :integer
+    Counter.find(:all).each{|c| c.calculate_current_tasks_num}
+  end
+
+  def self.down
+    drop_column :counters, :finished_tasks_num, :integer
+    drop_column :counters, :total_tasks_num, :integer
+  end
+end
